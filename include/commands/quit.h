423be2c427ef0df72a6de6862e193e7466c41250
yroussea <youenn.rousseau@hotmail.com>
2025-04-24 18:25:25 +0200

    fix bug: quit with 0 argv

--- include/commands/quit.h
+++ include/commands/quit.h
@@ -9,11 +9,11 @@ IRC_COMMAND_DEF(QUIT)
 {
 	const std::vector<str>	&argv = _parsingParam(command);
 
-	if (argv.size() != 1)
+	if (argv.size() > 1)
 		return ;
 
 	_send(client, "ERROR");
 	if (client.get_channelMap().size() != 0)
-		_broadcast("QUIT Quit: " + argv[0]);
+		_broadcast("QUIT Quit: " + (argv.size() ? argv[0] : ""));
 	IRC_FLAG_SET(client.get_flag(), IRC_CLIENT_EOF);
 }
